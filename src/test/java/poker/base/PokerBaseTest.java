d71158a351118360d73072bbe06fb7a2d7c8d1a2
Dennis Smiley <dennis.j.smiley@gmail.com>
2019-10-06 12:57:25 -0400

    Initial commit.

---
+++ src/test/java/poker/base/PokerBaseTest.java
@@ -0,0 +1,15 @@
+package poker.base;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PokerBaseTest {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void testPokerBase() {
+        logger.info("Hello from PokerBase tests");
+    }
+}
